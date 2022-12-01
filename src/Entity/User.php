c11734040a1c0c9c11d7fb9441ff2d3dbe6696fd
Jérémy FOURNIER <10160564+Jazzdak@users.noreply.github.com>
2022-12-01 11:29:32 +0100

    Jour 3

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,10 +3,13 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-class User
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -24,6 +27,14 @@ class User
 
     #[ORM\Column(length: 20)]
     private ?string $preferredChannel = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $password = null;
+
+    private ?string $plainpassword = null;
+
+    #[ORM\Column(type: Types::SIMPLE_ARRAY)]
+    private array $roles = [];
 
     public function getId(): ?int
     {
@@ -77,4 +88,80 @@ class User
 
         return $this;
     }
+
+    public function getRoles(): array
+    {
+        return $this->roles;
+    }
+
+    public function eraseCredentials()
+    {
+        $this->plainpassword = null;
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return $this->email;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string|null $password
+     */
+    public function setPassword(?string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @param array $roles
+     */
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    public function addRole(string $role): self
+    {
+        $this->roles[] = $role;
+
+        return $this;
+    }
+
+    public function removeRole(string $role): self
+    {
+        $key = array_search($role, $this->roles);
+        if(!is_null($key)){
+            unset($this->roles[$key]);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getPlainpassword(): ?string
+    {
+        return $this->plainpassword;
+    }
+
+    /**
+     * @param string|null $plainpassword
+     */
+    public function setPlainpassword(?string $plainpassword): self
+    {
+        $this->plainpassword = $plainpassword;
+
+        return $this;
+    }
+
 }
